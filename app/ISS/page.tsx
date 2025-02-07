43884f68926c32382965712d61feba5913c2973e
shanemiller500 <shanemiller500@gmail.com>
2025-02-06 20:28:11 -0700

    import the ISSTracker component with ssr: false for vercel issue

--- app/ISS/page.tsx
+++ app/ISS/page.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import ISSTracker from "./issTracker";
+"use client"; // Marking this as a client-side component
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import the ISSTracker component with ssr: false
+const ISSTracker = dynamic(() => import("./issTracker"), { ssr: false });
 
 const ISSTrackerPage = () => {
   return (
     <div>
-
       <ISSTracker />
     </div>
   );
