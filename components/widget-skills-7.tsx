80296ad9ebd1536e94c82a4dd9fca8a5032a154b
shanemiller500 <shanemiller500@gmail.com>
2025-02-08 14:13:49 -0700

    update email

--- components/widget-skills-7.tsx
+++ components/widget-skills-7.tsx
@@ -47,6 +47,21 @@ export default function WidgetSkills() {
           ></div>
         </li>
 
+        
+        <li className="flex justify-between items-center">
+          <div className="grow inline-flex mr-1 truncate">
+            <span className="text-indigo-500 mr-2">—</span>
+            <a className="font-aspekta font-[650] text-sm truncate">Websocket Integration</a>
+          </div>
+          <div
+            className="shrink-0 relative w-20 h-1.5 bg-slate-200 dark:bg-slate-700 before:absolute before:inset-0 before:bg-indigo-500 before:w-[70%]"
+            role="progressbar"
+            aria-valuenow={100}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          ></div>
+        </li>
+
     
       </ul>
     </div>
