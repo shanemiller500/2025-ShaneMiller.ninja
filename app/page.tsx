9d4027ef97320d5dcbc46a296c79f727f7f98e3a
shanemiller500 <shanemiller500@gmail.com>
2025-05-19 16:32:10 -0600

    testing again

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function AboutPage() {
         <div className="grow md:flex space-y-8 md:space-y-0 md:space-x-8 pt-12 md:pt-16 pb-16 md:pb-20">
           {/* Middle area */}
           <div className="grow">
+          <div className="max-w-[700px]">
             <section>
               <div className="overflow-hidden relative rounded-lg">
                 <div
@@ -89,6 +90,7 @@ export default function AboutPage() {
                 </div>
               </div>
             </section>
+          </div>
           </div>
 
           {/* Right sidebar */}
