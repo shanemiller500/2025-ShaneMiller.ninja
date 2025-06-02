9c10b9cfee57d80d1a80836af72bf1b4c84ab1f1
shanemiller500 <shanemiller500@gmail.com>
2025-06-02 16:52:30 -0600

    greeting

--- components/ConsoleGreeting.tsx
+++ components/ConsoleGreeting.tsx
@@ -9,12 +9,11 @@ export default function ConsoleGreeting() {
       'color:rgb(177, 34, 34)',         
     ].join(';')
 
-    console.clear()
     console.log('%c╔══════════════════════════════════════════════════════╗', style)
     console.log('%c║      Ah, a wild Dev appears...                       ║', style)
-    console.log('%c║      Welcome to the secret console lounge!           ║', style)
+    console.log('%c║      Welcome to the console lounge!                  ║', style)
     console.log('%c║      Sneaking around DevTools looking for secrets?   ║', style)
-    console.log('%c║      P.S. Escape? Alt-F4, feed the darkness.         ║', style)
+    console.log('%c║      Want to Escape? Alt-F4, feed the darkness.      ║', style)
     console.log('%c╚══════════════════════════════════════════════════════╝', style)
   }, [])
 
