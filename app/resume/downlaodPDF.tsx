8bd0dd1acdb0bf8cdf8fbf9ef5e1eb8a3b35017f
shanemiller500 <shanemiller500@gmail.com>
2026-01-27 17:12:29 -0700

    new resume

--- app/resume/downlaodPDF.tsx
+++ app/resume/downlaodPDF.tsx
@@ -11,7 +11,7 @@ export default function DownloadPDF() {
   return (
     <div className="flex flex-col items-center justify-center py-8">
       <a
-        href="/PDF/2025Resume.pdf"
+        href="/PDF/ShaneMillerResume.pdf"
         download
         onClick={handleDownloadClick}
         className="inline-flex items-center px-6 py-3 bg-indigo-500/50 dark:bg-indigo-900/40 text-gray-900 dark:text-white font-semibold rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-105 hover:shadow-xl focus:outline-none"
