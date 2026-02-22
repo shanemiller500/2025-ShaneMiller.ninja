0ff0f8e3f949866bdb9b66e0d2bcfe3b8d160545
shanemiller500 <shanemiller500@gmail.com>
2026-02-22 15:09:55 -0700

    fixed again

--- types/leaflet-terminator.d.ts
+++ types/leaflet-terminator.d.ts
@@ -1,5 +1,5 @@
 declare module 'leaflet-terminator' {
   import * as L from 'leaflet';
-  function leafletTerminator(options?: L.PolylineOptions): L.Polygon;
+  function leafletTerminator(date?: Date, options?: L.GeoJSONOptions): L.GeoJSON;
   export default leafletTerminator;
 }
